1638887d416f4bbe120f94ca0663c149d0e5a995
ErenAydogan <103932990+ErenAydogan@users.noreply.github.com>
2024-03-13 03:02:01 +0300

    PHP

--- Project_01/helpers.php
+++ Project_01/helpers.php
@@ -1,11 +1,37 @@
 <?php
     /*
     *Get the base path
-    * @param string $path
-    * @return string
     */
 
     function basePath($path = '') {
         return __DIR__ . '/' . $path;
     }
+
+
+    /*
+    *Load a view
+    */
+
+    function loadView($name) {
+        $viewPath = basePath("views/{$name}.view.php");
+
+        if (file_exists($viewPath)) {
+            require $viewPath;
+        } else {
+            echo "View '{$name} not found!'";
+        }
+    }
+
+    /*
+    * Load a partial
+    */
+    function loadPartial($name){
+        $partialPath = basePath("views/partials/{$name}.php");
+        if (file_exists($partialPath)) {
+            require $partialPath;
+        } else {
+            echo "Partial '{$name} not found!'";
+        }
+    }
+
 ?>
