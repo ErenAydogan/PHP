079ea272314c82a270ece08ea7f5a1411ce4ceb1
ErenAydogan <103932990+ErenAydogan@users.noreply.github.com>
2024-03-18 22:59:09 +0300

    PHP

--- Project_01/Router.php
+++ Project_01/Router.php
@@ -1,9 +1,52 @@
 <?php
-    $routes = require basePath('routes.php');
+    class Router {
+        protected $routes = [];
 
-    if(array_key_exists($uri, $routes)) {
-        require(basePath($routes[$uri]));
-    } else {
-        require basePath($routes['404']);
+        public function registerRoute($method, $uri, $controller){
+            $this -> routes[] = [
+                'method' => $method,
+                'uri' => $uri,
+                'controller' => $controller,
+            ];
+        }
+
+        //Adding a GET route
+        public function get($uri, $controller) {
+            $this -> registerRoute('GET', $uri, $controller);
+        }
+
+        //Adding a POST route
+        public function post($uri, $controller) {
+            $this -> registerRoute('POST', $uri, $controller);
+        }
+
+        //Adding a PUT route
+        public function put($uri, $controller) {
+            $this -> registerRoute('PUT', $uri, $controller);
+        }
+
+        //Adding a DELETE route
+        public function delete($uri, $controller) {
+            $this -> registerRoute('DELETE', $uri, $controller);
+        }
+
+        //Load error page
+        public function error($httpCode = 404){
+            http_response_code($httpCode);
+            loadView("error/{$httpCode}");
+            exit;
+        }
+
+
+        //Routing the request
+        public function route($uri, $method){
+            foreach($this->routes as $route){
+                if($route['uri'] === $uri && $route['method'] === $method){
+                    require basePath($route['controller']);
+                    return;
+                }
+            }
+            $this -> error();
+        }
     }
 ?>
